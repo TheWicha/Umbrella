4a3942aaf06da3f42cd5b67a69ab5f3ac71ba93e
TheWicha <kontakt.danielwisniewski@gmail.com>
2020-05-11 20:54:39 +0200

    Update Help component

--- client/src/components/Help.tsx
+++ client/src/components/Help.tsx
@@ -1,11 +1,52 @@
 import React from "react";
-import { Box } from "@material-ui/core";
+import { Container, Typography, TextField,Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { useSelector } from "react-redux";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Help: React.FC = () => {
+  const theme = useSelector((theme: any) => theme.getTheme);
+
+  const useStyles = makeStyles({
+    root: {
+      padding: "5em 3em",
+      display: "flex",
+      flexDirection: "column",
+      backgroundColor: theme.secondary,
+    },
+    head: {
+      paddingBottom: "1.1em",
+      fontWeight: 300,
+      color: "white",
+    },
+    form: {
+      padding: "1em",
+      "& > *": {
+        width: "50ch",
+      },
+      marginBottom:'9em'
+    },
+  });
+  const classes = useStyles();
   return (
-    <Box>
-      <h1>IM here to HELP YOU</h1>
-    </Box>
+    <Container maxWidth={"lg"} className={classes.root}>
+      <Container maxWidth="md">
+        <Typography variant="h4" className={classes.head}>
+          Need Help?
+        </Typography>
+        <form className={classes.form} noValidate autoComplete="off">
+          <TextField
+            id="filled-basic"
+            label={[<SearchIcon />, "Have a question?"]}
+            variant="filled"
+          />
+        </form>
+        <Typography variant="h4" className={classes.head}>
+          Can't find what you're looking for?
+        </Typography>
+        <Button variant="contained" color="primary">Contact Us</Button>
+      </Container>
+    </Container>
   );
 };
 
