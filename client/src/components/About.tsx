a0bbc589146bdb6ee8ffb0e68833929d42e1a831
TheWicha <kontakt.danielwisniewski@gmail.com>
2020-05-11 20:53:55 +0200

    Update About component

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -3,8 +3,6 @@ import { Container, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { useSelector } from "react-redux";
 import "typeface-roboto";
-import img from "../images/passion.jpg";
-import img2 from "../images/doge.jpg";
 
 const About: React.FC = () => {
   const theme = useSelector((theme: any) => theme.getTheme);
@@ -18,82 +16,58 @@ const About: React.FC = () => {
       backgroundColor: theme.secondary,
     },
     text: {
-      border: '1px solid white',
-      opacity: "0.7",
       fontFamily: "Akzidenz Grotesk BQ Light,Helvetica,Arial",
-      fontSize: "15px",
+      fontSize: "17px",
       color: "white",
-      padding: "3em",
+      padding: "1em",
     },
-    img: {
-      border: '1px solid white',
-      opacity: "0.9",
-      width: "50%",
-    },
-    container: { 
-      display: "flex", 
-      justifyContent: "center" 
-    },
-    logo: {
-      fontSize: "2.55em",
-      marginTop: "0.3em",
-      marginBottom: "2em",
-    },
+    head:{
+      paddingBottom: '1.1em',
+      fontWeight: 300,
+      color: "white"
+    }
+
   });
 
   const classes = useStyles();
 
   return (
     <Container className={classes.root} maxWidth="lg">
-      <Container maxWidth="md" className={classes.container}>
-        <Typography variant="h3" style={{ fontWeight: 300, color: "white" }}>
+      <Container maxWidth="md">
+        <Typography variant="h4" className={classes.head}>
           About Umbrella
         </Typography>
-      </Container>
-      <Typography className={classes.logo}>
-      </Typography>
-      <Container maxWidth="md" className={classes.container}>
-        <img
-          className={classes.img}
-          src={img}
-          alt="two people standing on sidewalk"
-        />
-        <Typography className={classes.text} variant="overline">
-          A prototype web application with where you can create, update and
+        <Typography variant="body1" className={classes.text}>
+          Umbrella is a prototype web application with where you can create, update and
           destroy products (post like), built with React, Redux, TypeScript,
           Yup, Formik, and other JS libraries to perform in the best possible
           way.
         </Typography>
-      </Container>
-      <Container
-        maxWidth="md"
-        className={classes.container}
-        style={{ flexDirection: "row-reverse" }}
-      >
-        <img
-          className={classes.img}
-          src={img2}
-          alt="two people standing on sidewalk"
-        />
-        <Typography className={classes.text} variant="overline">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-          maximus ex eleifend efficitur imperdiet. Curabitur dui elit, iaculis
-          vel enim in, interdum elementum ligula.
+        <br  />
+        <Typography variant="body1" className={classes.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+          aliquet velit quis enim aliquet, nec convallis tortor mollis. Proin
+          justo tortor, sollicitudin ac ultrices eu, cursus luctus magna. Donec
+          viverra sit amet nunc eu hendrerit. Maecenas condimentum nec nibh sit
+          amet lacinia. Quisque odio eros, dignissim quis arcu eget, maximus
+          posuere nibh. Nunc blandit elit neque, laoreet tincidunt nulla luctus
+          eu. Duis vitae ex id enim pulvinar luctus. Ut commodo sed sem id
+          varius. Duis nec magna ut ante efficitur ultricies. Nulla eget iaculis
+          felis, ac suscipit enim. Cras quis consequat diam. Maecenas sit amet
+          ex dictum nibh tincidunt consectetur eu quis lectus.
+        </Typography>
+        <hr style={{background: 'black', opacity: '0.3', width:'96%'}} />
+        <Typography variant="body1" className={classes.text}>
+        Donec quis
+          accumsan justo. In hac habitasse platea dictumst. Nullam laoreet
+          vulputate felis. Etiam neque nunc, porta sed urna a, varius pharetra
+          lorem. Etiam sapien ligula, congue ut consequat faucibus, consequat
+          non mi. Sed nibh ipsum, dapibus in felis a, aliquet molestie leo.
+          Maecenas ac sapien eu orci tristique facilisis. Suspendisse imperdiet
+          enim turpis, ac sollicitudin elit eleifend vel. Etiam ullamcorper non
+          velit ac euismod. Cras scelerisque ut orci id lobortis.
         </Typography>
       </Container>
-      <Typography style={{ fontSize: "0.5em", alignSelf: "start" }}>
-        Images are from{" "}
-        <a href="https://www.unsplash.com" target="_blank" rel="noopener noreferrer">
-          unsplash.com
-        </a>
-        <br />
-        <a href="https://unsplash.com/photos/TamMbr4okv4"  rel="noopener noreferrer" target="_blank">
-          Photo 1
-        </a>
-        <a href="https://unsplash.com/photos/f81ym3dE5N4"  rel="noopener noreferrer" target="_blank">
-          Photo 2
-        </a>
-      </Typography>
     </Container>
   );
 };
