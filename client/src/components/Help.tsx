ca5e4a2f12d572f484b3744611b2bc14ec8d905e
TheWicha <kontakt.danielwisniewski@gmail.com>
2020-05-11 21:55:02 +0200

    Update Help component

--- client/src/components/Help.tsx
+++ client/src/components/Help.tsx
@@ -1,20 +1,23 @@
 import React from "react";
-import { Container, Typography, TextField,Button } from "@material-ui/core";
+import { Container, Typography, TextField, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { useSelector } from "react-redux";
 import SearchIcon from "@material-ui/icons/Search";
+import img from "../images/maintenance.jpg";
 
 const Help: React.FC = () => {
   const theme = useSelector((theme: any) => theme.getTheme);
 
   const useStyles = makeStyles({
     root: {
-      padding: "5em 3em",
+      padding: "8em 3em",
       display: "flex",
       flexDirection: "column",
       backgroundColor: theme.secondary,
     },
     head: {
+      textShadow: "1px 1px black",
+      marginTop: "1em",
       paddingBottom: "1.1em",
       fontWeight: 300,
       color: "white",
@@ -24,19 +27,24 @@ const Help: React.FC = () => {
       "& > *": {
         width: "50ch",
       },
-      marginBottom:'9em'
+      marginBottom: "9em",
+    },
+    background: {
+      position: "absolute",
+      opacity: "0.7",
+      height: "55%",
+      marginLeft: "7.2em",
     },
   });
   const classes = useStyles();
   return (
     <Container maxWidth={"lg"} className={classes.root}>
-      <Container maxWidth="md">
+      <Container maxWidth="lg" style={{ zIndex: 1, marginLeft: "8em" }}>
         <Typography variant="h4" className={classes.head}>
           Need Help?
         </Typography>
         <form className={classes.form} noValidate autoComplete="off">
           <TextField
-            id="filled-basic"
             label={[<SearchIcon />, "Have a question?"]}
             variant="filled"
           />
@@ -44,8 +52,11 @@ const Help: React.FC = () => {
         <Typography variant="h4" className={classes.head}>
           Can't find what you're looking for?
         </Typography>
-        <Button variant="contained" color="primary">Contact Us</Button>
+        <Button variant="contained" color="primary" style={{ margin: "1em" }}>
+          Contact Us
+        </Button>
       </Container>
+      <img src={img} className={classes.background} alt="maintenance" />
     </Container>
   );
 };
